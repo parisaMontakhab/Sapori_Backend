a83a61845a730375fa5f26b29d1b2d98f154a8d8
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-15 23:20:47 +0200

    feat:Complete product API with search filter and pagination

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,24 +4,31 @@ const Product = require("../models/productModel");
 
 exports.getAllProducts = async (req, res) => {
   try {
-    let products;
+    let query;
 
-    //category
     if (req.query.search) {
-      products = await Product.find({
+      query = Product.find({
         name: { $regex: req.query.search, $options: "i" },
       });
     } else if (req.query.category) {
-      products = await Product.find({
+      query = Product.find({
         category: req.query.category,
       });
     } else {
-      products = await Product.find();
+      query = Product.find();
     }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 12;
+    const skip = (page - 1) * limit;
+
+    const products = await query.skip(skip).limit(limit);
 
     res.status(200).json({
       success: true,
       results: products.length,
+      page,
+      limit,
       data: {
         products,
       },
