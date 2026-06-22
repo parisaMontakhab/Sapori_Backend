23993f7b0a85e2b8d9b54088c96cbfd098d9de4c
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-22 11:24:19 +0200

    feat: Created userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "user must have a name"],
+      trim: true,
+      minlength: [2, "user name must be at least 2 characters"],
+      maxlength: [80, "user name must be less than 80 characters"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "user must have a password"],
+      minlength: [8, "password must be at least 8 characters"],
+      select: false,
+    },
+
+    role: {
+      type: String,
+      required: [true, "user must have a role"],
+      enum: {
+        values: ["admin", "user"],
+        message: "Invalid user role",
+      },
+    },
+
+    email: {
+      type: String,
+      required: [true, "user must have an email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
