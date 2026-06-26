8dc8aa177f366b21e070c5edd2d30247de533add
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-26 11:55:29 +0200

    feat: created user route to implement update delete and get user

---
+++ src/controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res
+    .status(200)
+    .json({ status: "success", result: users.length, data: { users } });
+});
+
+exports.getUserByID = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new AppError("not found user with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { user },
+  });
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+  res.status(200).json({ status: "success", data: { user } });
+});
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id, req.body);
+
+  if (!user) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const { signup, login } = require("../controllers/authController");
+const {
+  getAllUsers,
+  getUserByID,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 
 router.post("/login", login);
+
+router.route("/").get(getAllUsers);
+
+router.route("/:id").get(getUserByID).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
