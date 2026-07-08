b618b0366c82a75e998c2bd70c9ea305770a2170
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-07-08 14:51:57 +0200

    refactor : deleted the CartRoutes and controllers

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const hpp = require("hpp");
 const { apiLimiter } = require("./middleware/rateLimiter");
 const productRouter = require("./routes/productRoutes");
 const userRouter = require("./routes/userRoutes");
-const cartRouter = require("./routes/cartRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -45,7 +44,6 @@ app.use(hpp());
 //Routes
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/cart", cartRouter);
 
 app.get("/", (req, res) => {
   // Health check route
--- src/routes/cartRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { addToCart } = require("../controllers/cartController");
-const router = express.Router();
-
-router.route("/:productId").post(addToCart);
-
-module.exports = router;
