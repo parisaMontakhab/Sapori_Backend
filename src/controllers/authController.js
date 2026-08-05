16cbe9266b364f3414cd3593399f06572ef68b91
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-08-05 12:51:32 +0200

    feat : implemented Email class for divers subject

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-const sendEmail = require("../utils/email");
+const Email = require("../utils/email");
 const crypto = require("crypto");
 
 const signToken = (id) => {
@@ -45,6 +45,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+
+  const url = `${req.protocol}://${req.get("host")}/me`;
+  await new Email(newUser, url).sendWelcome();
 
   createSendToke(newUser, 201, res);
 });
@@ -144,11 +147,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password?Submit a PATCH request with your new password and passwordConfirm to:${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: "Your password reset token (is valid for 10 min)",
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: "Your password reset token (is valid for 10 min)",
+    //   message,
+    // });
 
     res.status(200).json({
       status: "success",
