a8feb7dc3c668bc81ab79b89bb774a04147c4161
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-08-13 19:59:17 +0200

    fix : fixed the getAllReviews function

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -51,7 +51,23 @@ exports.checkReviewOwner = catchAsync(async (req, res, next) => {
 
 exports.createReview = factory.createOne(Review);
 
-exports.getAllReviews = factory.getAll(Review);
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const filter = {};
+
+  if (req.params.productId) {
+    filter.product = req.params.productId;
+  }
+
+  const reviews = await Review.find(filter);
+
+  res.status(200).json({
+    status: "success",
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
 
 exports.deleteReview = factory.deleteOne(Review);
 
